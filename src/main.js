39faaf3f3a46632bc49b8e8a1d277659374e2250
huuthiendev <huuthiendev@gmail.com>
2022-05-26 22:34:38 +0700

    update main flow

---
+++ src/main.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const { argv, SCRIPT_NAME } = require('./cli-command');
+const TRANSACTION_FILE = 'transactions.csv';
+
+function getPorfolio(argv) {
+    console.log('Calculating the portfolio ...')
+    var tokenPorfolios = {};
+
+    fs.createReadStream(TRANSACTION_FILE)
+        .pipe(csv())
+        .on('data', data => {
+            // Token & Date
+            if (argv.token &&
+                argv.date &&
+                data.token === argv.token &&
+                data.timestamp >= argv.fromDate &&
+                data.timestamp <= argv.toDate) {
+                tokenCounter(tokenPorfolios, data);
+            }
+            // Date
+            else if (argv.date &&
+                data.timestamp >= argv.fromDate &&
+                data.timestamp <= argv.toDate) {
+                tokenCounter(tokenPorfolios, data);
+            }
+            // Token
+            else if (argv.token && data.token === argv.token) {
+                tokenCounter(tokenPorfolios, data);
+            }
+            // No options
+            else {
+                tokenCounter(tokenPorfolios, data);
+            }
+        })
+        .on('end', async function () {
+            if (!Object.keys(tokenPorfolios).length) {
+                console.log('No matching results');
+                return;
+            }
+
+            for (let index = 0; index < Object.keys(tokenPorfolios).length; index++) {
+                const tokenPorfolio = tokenPorfolios[index];
+            }
+            console.log('Token portfolio in USD: ', tokenPorfolios);
+        })
+        .on('error', function (err) {
+            console.log('ERROR: ', err);
+        });
+}
+
+function tokenCounter(tokenPorfolios, data) {
+    if (!tokenPorfolios[data.token]) {
+        tokenPorfolios[data.token] = data.transaction_type === 'DEPOSIT'
+            ? Number(data.amount)
+            : Number(-data.amount);
+    } else {
+        tokenPorfolios[data.token] = data.transaction_type === 'DEPOSIT'
+            ? Number(tokenPorfolios[data.token]) + Number(data.amount)
+            : Number(tokenPorfolios[data.token]) - Number(data.amount);
+    }
+}
+
+if (argv._.length && argv._[0] === SCRIPT_NAME) {
+    getPorfolio(argv);
+}
